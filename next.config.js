2f6473bf7c9cf896ca592d6cc3842b12ba89d17f
mssystem1 <mv.shishkov1987@gmail.com>
2025-08-21 18:13:02 +0400

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Ignore TypeScript & ESLint errors **only during builds** (keeps dev DX)
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-};
-module.exports = nextConfig;
