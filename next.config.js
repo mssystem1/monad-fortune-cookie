c1952b6a008e0dfd283f69e52276f8534148bd41
mssystems1 <mv.shishkov1987@gmail.com>
2025-08-29 20:22:11 +0400

    updated next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // ✅ Make sure Next writes to a folder, not a file
+  distDir: '.next',
+
   typescript: { ignoreBuildErrors: true },
   eslint: { ignoreDuringBuilds: true },
 };
